66c5b4052ed75f968387d47cf6b3a6362e8ddfaa
c0ll3cti0n <16981914+c0ll3cti0n@users.noreply.github.com>
2019-11-14 11:24:16 +0530

    Delete 2.sh

--- 2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo Please enter your number
-read varname
-
-while :
-do
-python btc* $varname && python doge* $varname && python ltc* $varname && python zec* $varname
-sleep 600
-done

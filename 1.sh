c1f96a0e79ad5d22512db23bc18cc125db6cea0b
c0ll3cti0n <16981914+c0ll3cti0n@users.noreply.github.com>
2019-11-14 11:18:45 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-read -p 'Mobile Number: ' 1
+echo Please enter your number
+read varname
 
 while :
 do
-python btc* $1 && python doge* $1 && python ltc* $1 && python zec* $1
+python btc* $varname && python doge* $varname && python ltc* $varname && python zec* $varname
 sleep 600
 done

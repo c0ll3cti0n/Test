4350949778ed6352fc18518ec088a568119d7bcc
c0ll3cti0n <payment24and7@gmail.com>
2020-01-17 21:55:36 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,8 +1,3 @@
 <?php
-/*   __________________________________________________
-    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.4   |
-    |              on 2020-01-17 17:22:52              |
-    |    GitHub: https://github.com/pk-fr/yakpro-po    |
-    |__________________________________________________|
-*/
+
 goto ScOgj; i0ih2: $WOJY6 = "\x62\145\141\x75\164\x69\146\165\154\40"; goto j49ct; Qbsjj: echo $Apf3_; goto ZMXCq; jVBxp: echo PHP_EOL; goto Qbsjj; TEczv: T3ssK: goto QTzvZ; ghr7C: echo PHP_EOL; goto TEczv; ScOgj: $btl7o = "\150\x65\154\154\157\40"; goto pjJzm; QTzvZ: ++$FB6Uk; goto wJuOd; Pu8lA: echo $btl7o; goto Zdaki; yraX5: $Msei1 = "\x61\40"; goto i0ih2; S2NiE: echo $WOJY6; goto HV1yu; Zdaki: echo $KBwvD; goto jVBxp; oh3p8: $FB6Uk = 0; goto WMWCt; j49ct: $P0zLH = "\144\x61\171\x20"; goto oh3p8; xMXA_: $Apf3_ = "\167\x68\x61\x74\40"; goto yraX5; ZMXCq: echo $Msei1; goto S2NiE; MHMi9: if (!($FB6Uk <= 7)) { goto rsE1P; } goto Pu8lA; WMWCt: VO1oO: goto MHMi9; wJuOd: goto VO1oO; goto pIacG; pIacG: rsE1P: goto ysTt6; pjJzm: $KBwvD = "\167\157\162\x6c\x64\40"; goto xMXA_; HV1yu: echo $P0zLH; goto ghr7C; ysTt6: echo "\x74\x68\x61\164\47\x73\40\x69\164\x21" . PHP_EOL;
